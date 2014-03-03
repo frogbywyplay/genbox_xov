7521d5933adb969443b35420643c4ce40cd4bf4a
ptisserand <ptisserand@wyplay.com>
2014-03-03 19:15:46 +0100

    setup.py: updated for next version

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ def find_packages(dir):
 
 setup(
     name = "xov",
-    version = "2.1.3",
+    version = "2.1.4",
     description = "Overlays tool management for genbox",
     author = "Wyplay",
     author_email = "noreply@wyplay.com",
